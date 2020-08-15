8e0ac12eebe85e7870c1672725b6be7a373d980a
MartinsMessias <messiasads2017@gmail.com>
2020-08-14 21:19:05 -0500

    Updates

---
+++ precache-manifest.18e89ded58c1f8f0327299428fda4332.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0364b8ff83d5eeb8d9f3d92b856a3c4b",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "1e770516b03e82d91ad0",
+    "url": "/social/static/css/main.34f6cb6f.chunk.css"
+  },
+  {
+    "revision": "8f46114222a8ec55fc63",
+    "url": "/social/static/js/2.ec5eef51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social/static/js/2.ec5eef51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e770516b03e82d91ad0",
+    "url": "/social/static/js/main.9eb68551.chunk.js"
+  },
+  {
+    "revision": "e2a05e4aa7d197d7ff98",
+    "url": "/social/static/js/runtime-main.1f066a7b.js"
+  },
+  {
+    "revision": "63d6ecd78662fef9b45b21bd304153c8",
+    "url": "/social/static/media/aws.63d6ecd7.png"
+  },
+  {
+    "revision": "41738a595725c3fa8a88378d44bba0aa",
+    "url": "/social/static/media/bg.41738a59.png"
+  },
+  {
+    "revision": "e9663d7b9673c4e4ea867080d9cc13ec",
+    "url": "/social/static/media/css.e9663d7b.png"
+  },
+  {
+    "revision": "00202c740bb7f67388595ac36ada4e28",
+    "url": "/social/static/media/dart.00202c74.png"
+  },
+  {
+    "revision": "d21f1eecaeaab081ba7efec1721c0712",
+    "url": "/social/static/media/flutter.d21f1eec.png"
+  },
+  {
+    "revision": "b23d8b67ed5d68a4e3c2bc5ac9786f97",
+    "url": "/social/static/media/git.b23d8b67.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/social/static/media/html.664a8c00.png"
+  },
+  {
+    "revision": "02d5846bdc5a260825018f3686f6d5a4",
+    "url": "/social/static/media/js.02d5846b.png"
+  },
+  {
+    "revision": "f3205522465315a859895ca51c8ed46d",
+    "url": "/social/static/media/json.f3205522.png"
+  },
+  {
+    "revision": "dd90baf33e447b60c51de076efedc7de",
+    "url": "/social/static/media/linux.dd90baf3.png"
+  },
+  {
+    "revision": "07ed078bff19acc7fa31fefa2f90affa",
+    "url": "/social/static/media/mongodb.07ed078b.png"
+  },
+  {
+    "revision": "5886aa9761d099ff128c4ac99a78176f",
+    "url": "/social/static/media/mysql.5886aa97.png"
+  },
+  {
+    "revision": "fd6984368ba75ec90b8f9fe0cae05566",
+    "url": "/social/static/media/python.fd698436.png"
+  },
+  {
+    "revision": "8ab12c496de37bfd940ae032c64aafa4",
+    "url": "/social/static/media/react.8ab12c49.png"
+  },
+  {
+    "revision": "439c353dd7e0cc228dc888101b396aa4",
+    "url": "/social/static/media/rest.439c353d.png"
+  },
+  {
+    "revision": "6ef42a8da411c603c041dbad54d6fc1d",
+    "url": "/social/static/media/sql.6ef42a8d.png"
+  }
+]);
