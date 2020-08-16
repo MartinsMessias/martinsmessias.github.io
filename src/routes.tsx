d13b6dd7e4d73da26e0dcd823ad84253e26be10d
MartinsMessias <messiasads2017@gmail.com>
2020-08-15 23:04:49 -0500

    My website in React

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, HashRouter } from "react-router-dom";
 import SobrePage from "./Pages/Sobre";
 import Portifolio from "./Pages/Portifolio";
 import ContatosPage from "./Pages/Contatos";
@@ -8,13 +8,13 @@ import Footer from "./Components/Footer";
 
 function Routes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
-      <Route path="/social" exact component={SobrePage} />
-      <Route path="/portifolio" component={Portifolio} />
-      <Route path="/contatos" component={ContatosPage} />
+      <Route path="/" exact component={SobrePage} />
+      <Route path="/portifolio/" component={Portifolio} />
+      <Route path="/contatos/" component={ContatosPage} />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
