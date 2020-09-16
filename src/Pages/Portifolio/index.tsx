54d29a91e72e0ca831efccf1cb649bd6c8a2c4d0
MartinsMessias <messiasads2017@gmail.com>
2020-09-15 22:25:17 -0500

    Update de setembro

--- src/Pages/Portifolio/index.tsx
+++ src/Pages/Portifolio/index.tsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+import Loader from "react-loader-spinner";
 
 // Personalização
 import Card from "../../Components/Card";
@@ -22,17 +25,21 @@ const Portifolio = () => {
           <div className="container">
             <h1 className="section-title">Portfólio</h1>
             <div id="cards-section">
-              {projects.map((project) => {
-                if (project["fork"] == 0)
-                  return (
-                    <Card
-                      title={project["name"]}
-                      description={project["description"]}
-                      link={project["html_url"]}
-                      lang={project["language"]}
-                    />
-                  );
-              })}
+              {projects.length > 0 ? (
+                projects.map((project) => {
+                  if (project["fork"] == 0)
+                    return (
+                      <Card
+                        title={project["name"]}
+                        description={project["description"]}
+                        link={project["html_url"]}
+                        lang={project["language"]}
+                      />
+                    );
+                })
+              ) : (
+                <Loader type="Grid" color="#0000030" height={80} width={80} />
+              )}
             </div>
           </div>
         </section>
--- src/Pages/Sobre/index.tsx
+++ src/Pages/Sobre/index.tsx
@@ -33,7 +33,36 @@ const SobrePage = () => {
         <section>
           <div className="container">
             <h1 className="section-title">Skills</h1>
-            Em breve!
+            <div id="skills-section" className="skill-list">
+              <div className="skill-col">
+                <h3 className="skill-level">Intermediário</h3>
+                <p className="skill-title">Python</p>
+                <p className="skill-title">Django</p>
+                <p className="skill-title">JavaScript</p>
+                <p className="skill-title">RESTFull API</p>
+                <p className="skill-title">HTML5</p>
+                <p className="skill-title">CSS3</p>
+                <p className="skill-title">MySQL</p>
+                <p className="skill-title">Docker</p>
+                <p className="skill-title">Git</p>
+                <p className="skill-title">Heroku</p>
+                <p className="skill-title">Linux</p>
+              </div>
+              <hr className="div-line" />
+              <div className="skill-col">
+                <h3 className="skill-level">Básico</h3>
+                <p className="skill-title">ReactJS</p>
+                <p className="skill-title">Web Scraping</p>
+                <p className="skill-title">Flutter</p>
+                <p className="skill-title">TypeScript</p>
+                <p className="skill-title">MongoDB</p>
+                <p className="skill-title">Windows Server</p>
+                <p className="skill-title">AWS EC2</p>
+                <p className="skill-title">AWS Lambda</p>
+                <p className="skill-title">S3</p>
+                <p className="skill-title">Elastic Beanstack</p>
+              </div>
+            </div>
           </div>
         </section>
       </main>
